88aaf31622e1994db13439bf6af719592bc454a7
Sharath Vashisht <sharathvashisht@gmail.com>
2017-02-26 09:17:05 +0530

    Adding constructor to second file

--- secondfile.java
+++ secondfile.java
@@ -1,4 +1,9 @@
 public class secondfile{
+	
+	public secondfile(){
+		System.out.println("Adding constructor");
+	}
+	
 	public static void main( String [] args ){
 		System.out.println("Testing second File");		
 	}
